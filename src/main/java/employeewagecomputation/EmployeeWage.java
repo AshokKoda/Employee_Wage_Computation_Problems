a4e2f2fb731f76cb634f9226562f1541d11fe118
Ashok Koda <kodaashok60@gmail.com>
2022-02-17 17:25:45 +0530

    Added:Employee attendences

--- src/main/java/employeewagecomputation/EmployeeWage.java
+++ src/main/java/employeewagecomputation/EmployeeWage.java
@@ -4,6 +4,15 @@ public class EmployeeWage {
 
 	public static void main(String[] args) {
 		System.out.println("----------Welcome to Employee Wage Computation-----------");
+		
+		//UC1 - Employee Attendances
+		double empCheck  = Math.floor(Math.random()* 10) %2;
+		if (empCheck == 1){
+            System.out.println("Employee is present");
+        }
+        else{
+            System.out.println("Employee is absent");
+        }
 
 	}
 
