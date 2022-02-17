aa8ee193ee80a10370749a73a018d70363fa5c3d
Ashok Koda <kodaashok60@gmail.com>
2022-02-17 17:40:35 +0530

    Added:Calculate employee wage

---
+++ src/main/java/employeewagecomputation/CalculateEmployeeWage.java
@@ -0,0 +1,23 @@
+package employeewagecomputation;
+
+public class CalculateEmployeeWage {
+
+	public static void main(String[] args) {
+		int IS_FULL_TIME = 1;
+        int EMP_WAGE_PER_HR = 20;
+        int FULL_DAY_HR = 8;
+        int empWage = 0;
+        int empHrs = 0;
+        double empCheck = Math.floor(Math.random() * 10) % 2;
+        if (empCheck == IS_FULL_TIME ){
+            empHrs= FULL_DAY_HR;
+        }else{
+            empHrs=0;
+        }
+
+        empWage = empHrs * EMP_WAGE_PER_HR;
+        System.out.println("Employee Wage: " + empWage);
+
+	}
+
+}
--- src/main/java/employeewagecomputation/EmployeeWage.java
+++ src/main/java/employeewagecomputation/EmployeeWage.java
@@ -4,15 +4,14 @@ public class EmployeeWage {
 
 	public static void main(String[] args) {
 		System.out.println("----------Welcome to Employee Wage Computation-----------");
-		
-		//UC1 - Employee Attendances
-		double empCheck  = Math.floor(Math.random()* 10) %2;
-		if (empCheck == 1){
-            System.out.println("Employee is present");
-        }
-        else{
-            System.out.println("Employee is absent");
-        }
+
+		// UC1 - Employee Attendances
+		double empCheck = Math.floor(Math.random() * 10) % 2;
+		if (empCheck == 1) {
+			System.out.println("Employee is present");
+		} else {
+			System.out.println("Employee is absent");
+		}
 
 	}
 
